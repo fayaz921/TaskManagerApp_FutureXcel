0f4473a806d51b99dc7ec72a93d91f6d9550c681
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-18 21:16:30 +0500

    Create TaskItem.cs

---
+++ TaskManagerApp_FutureXcel/Models/TaskItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerApp_FutureXcel.Models
+{
+    public class TaskItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; } =string.Empty;
+
+        public string? Description { get; set; }
+
+        [Required]
+        public TaskStatus Status { get; set; }  
+
+        public DateTime? DueDate { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
