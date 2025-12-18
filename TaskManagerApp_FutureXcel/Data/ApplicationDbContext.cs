73bf15295411a220051ee0abbc16468354cc959c
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-18 21:16:16 +0500

    Create ApplicationDbContext.cs

---
+++ TaskManagerApp_FutureXcel/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerApp_FutureXcel.Models;
+
+namespace TaskManagerApp_FutureXcel.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
